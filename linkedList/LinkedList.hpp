bc10dff0b75fc54aa268fd26cc30a3731958d646
Jacob Prichard <jlprich3@ku.edu>
2019-09-13 12:21:09 -0500

    fixed search

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -37,12 +37,15 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
-
-	/** TODO
-		Fix this method
-	*/
-
-	return(isFound);
+	for(int i=0;i<m_size;i++)
+	{
+		if(temp->getValue()==value)
+		{
+			return(true);
+		}
+		temp = temp->getNext();
+	}
+	return(false);
 }
 
 template <typename T>
